27b82d849f63e1f943888cc9ce93d2cfda90e0e5
Charles McCullough <charles@charlesmccullough.com>
2021-02-27 11:54:51 -0600

    End of Unit 154

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -8,6 +8,7 @@ using API.Dtos;
 using AutoMapper;
 using System.Linq;
 using AutoMapper.QueryableExtensions;
+using API.Helpers;
 
 namespace API.Data
 {
@@ -57,11 +58,14 @@ namespace API.Data
                     .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
                     .SingleOrDefaultAsync();
         }
-        public async Task<IEnumerable<MemberDto>> GetMembersAsync()
+        public async Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams)
         {
-            return await _context.Users
+                 var query = _context.Users
                     .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
-                    .ToListAsync();
+                    .AsNoTracking();
+
+                    return await PagedList<MemberDto>.CreateAsync(query, userParams.PageNumber, userParams.PageSize);
+                    
         }
     }
 }
