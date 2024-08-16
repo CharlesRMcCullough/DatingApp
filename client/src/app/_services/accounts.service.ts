56284c63499d8e65bdc300bffd8cb240d4280a55
Charles McCullough <charles@charlesmccullough.com>
2024-08-16 17:42:55 -0500

    Home page skelton complete

--- client/src/app/_services/accounts.service.ts
+++ client/src/app/_services/accounts.service.ts
@@ -1,5 +1,7 @@
-import {inject, Injectable} from '@angular/core';
+import {inject, Injectable, signal} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {User} from "../_models/User";
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +10,21 @@ export class AccountsService {
   private http = inject(HttpClient);
 
   baseUrl = 'http://localhost:5000/api/';
+  currentUser = signal<User | null>(null);
 
   login(model: any) {
-      return this.http.post(this.baseUrl + 'account/login', model);
+      return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
+          map(user => {
+            if (user) {
+              localStorage.setItem('user', JSON.stringify(user));
+              this.currentUser.set(user);
+            }
+          })
+      );
+    }
+
+    logout() {
+      localStorage.removeItem('user');
+      this.currentUser.set(null);
     }
 }
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,28 +1,32 @@
 import {Component, inject} from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {AccountsService} from "../_services/accounts.service";
+import {BsDropdownModule} from "ngx-bootstrap/dropdown";
 
 @Component({
   selector: 'app-nav',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    BsDropdownModule
   ],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
-  loggedIn = false;
-  private accountService = inject(AccountsService);
+
+  accountService = inject(AccountsService);
   model: any = {};
 
   login() {
     this.accountService.login(this.model).subscribe({
       next: response => {
         console.log(response);
-        this.loggedIn = true;
       },
       error: error => console.log(error)
     })
+  }
+  logout() {
+    this.accountService.logout();
   }
 }
